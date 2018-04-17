2fc2fcdf42ed5ebc9a2aab7ada29e7b2c70f7bfd
ErikBoesen <me@erikboesen.com>
2018-04-16 21:38:13 -0400

    Remove iTunes files

--- clean.sh
+++ clean.sh
@@ -4,3 +4,7 @@ rm -rf /Library/Application\ Support/{Logic,GarageBand}
 rm -rf /Applications/GarageBand.app
 
 rm -rf /Applications/Microsoft\ Silverlight
+
+rm -rf /Library/iTunes
+
+# TODO: Optionally remove McAfee
